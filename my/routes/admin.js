672c30d41de124ab93c51a86237bf9e1ba6563c3
lxndrrud <lxndrrud@gmail.com>
2021-08-08 13:07:10 +0300

    code review troubleshooting #1

--- my/routes/admin.js
+++ my/routes/admin.js
@@ -30,9 +30,8 @@ router.post('/user/create', adminAuth, async function(req, res, next){
       surname: form.surname,
       role_title: form.role_title
     };
-    var checkArray = await mydb.checkUser(data.login);
 
-    if (checkArray[0]) // можно упростить
+    if ((await mydb.checkUser(data.login))[0]) // можно упростить
       return res.status(409).send('Пользователь с таким логином уже существует!');
 
     if (data.password === data.passwordConfirm){
@@ -144,7 +143,7 @@ router.get('/test/edit/:test_id', adminAuth, async function(req, res, next){
 
 router.post('/test/edit/:test_id', adminAuth, async function(req, res, next){
   try{
-    form = req.body; // забыл объявить переменную
+    const form = req.body; // забыл объявить переменную
     if (req.params.test_id){
       let context = {
         creator_login: form.creator_login,
@@ -227,13 +226,9 @@ router.post('/result/edit/:result_id', adminAuth, async function(req, res, next)
 
 router.get('/result/delete/:result_id', adminAuth, async function(req, res, next){
   var result = (await mydb.getResultByResultId(req.params.result_id))[0];
-  console.log(result); // Не забываем убирать, в проде будет лишняя нагрузка
-  if (!result){ // можно упростить, как указал ниже, а так же пробел перед фигурной скобкой
-    res.redirect('/admin');
-  }
-  else{
-    res.render('admin/deleteResult', {result: result});
-  }
+  // можно упростить, как указал ниже, а так же пробел перед фигурной скобкой
+  if (!result) res.redirect('/admin');
+  else res.render('admin/deleteResult', {result: result});
 });
 
 router.post('/result/delete/:result_id', adminAuth, async function(req, res, next){
@@ -253,12 +248,10 @@ router.post('/search/tests', adminAuth, async function(req, res, next){
   var form = req.body;
   var tests = [];
   var context = {};
-  if (form.login && !form.test_id){
+  if (form.login && !form.test_id) 
     tests = await mydb.getTestsByUserLogin(form.login);
-  }
-  else if (form.test_id){
+  else if (form.test_id) 
     tests = await mydb.getTestById(form.test_id);
-  }
   if (!tests){
     context = {
       tests: []
